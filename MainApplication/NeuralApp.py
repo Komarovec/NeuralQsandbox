144ae8b3f7701caba1386209277fcc64ff722378
Denis <komarovec1@gmail.com>
2019-07-12 22:16:45 +0200

    Designing GUI with Kivy
    Trying to build simple GUI with Kivy framework. Picture of the city is only for testing purposes.

--- MainApplication/NeuralApp.py
+++ MainApplication/NeuralApp.py
@@ -8,15 +8,27 @@ import kivy
 kivy.require('1.0.7')
 
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class MainMenu(Widget):
+#Window classes
+class MainMenuWindow(Screen):
     pass
 
+#Screen manager
+kv = Builder.load_file("templates/neural.kv")
+sm = ScreenManager()
+screens = [MainMenuWindow(name="mainmenu")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "mainmenu"
+
+#Main class
 class NeuralApp(App):
-    kv_directory = 'templates'
     def build(self):
-        return MainMenu()
+        return sm
 
 if __name__ == '__main__':
     NeuralApp().run()
